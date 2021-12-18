fcdd9376f9a49288cb0390a073da2cdbc0bd774c
pedrecho <pedrecho.flash@gmail.com>
2021-12-18 22:56:52 +0300

    v 1.1

--- match_statistic.py
+++ match_statistic.py
@@ -2,25 +2,32 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 import time
 
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 
 def match_result(href):
     driver.get(href)
     time.sleep(1)
     html = driver.page_source
     soup = BeautifulSoup(html, features='html.parser')
-    teams = list(map(lambda x: x.find("div",class_="b-title bt16 bold").get_text(), soup.find_all("div",class_="team-name")))
-    score = soup.find_all("div", class_="score")[2].find("div",class_="b-title bt16 bold").get_text().split(':')
+    teams = list(map(lambda x: x.find("div", class_="b-title bt16 bold").get_text(), soup.find_all("div", class_="team-name")))
+    score = soup.find_all("div", class_="score")[2].find("div", class_="b-title bt16 bold").get_text().split(':')
+    # print(score)
     teams.append(score[0])
     teams.append(score[1])
     return teams
 
-def all_matches_results():
-    links = open('links2.txt', 'r').read().split('\n')
-    f = open('matches_result.txt','w')
+
+def all_matches_results(fileName, fileLinks):
+    links = open(fileLinks + '.txt', 'r').read().split('\n')
+    f = open(fileName + '.txt', 'w')
     for link in links:
-        f.write('; '.join(match_result(link)))
+        f.write(','.join(match_result(link)) + '\n')
     f.close()
 
+
+all_matches_results('matches', 'links3')
